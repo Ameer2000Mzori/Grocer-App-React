d8360d9e1fd899883b157aad454112dff3eef259
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-27 11:45:43 +0100

    added html tree

---
+++ src/component/Grocery.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Grocery = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>Grocery List</h1>
+          <div>
+            <ul>
+              <li>
+                <input type="checkbox" />
+                <p>Hallo world</p>
+                <button>Delete</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
