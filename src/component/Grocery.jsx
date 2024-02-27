f91a0830b9838fcc50b1ac9057e2a4b40eaf5264
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-27 12:11:03 +0100

    added some more logic to the project

--- src/component/Grocery.jsx
+++ src/component/Grocery.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Data } from './hooks/Data.jsx'
 
 export const Grocery = () => {
+  const [ourData, setOurData] = useState([])
+  const [checkBox, setCheckBox] = useState(false)
+
+  useEffect(() => {
+    const newData = Data()
+    console.log(newData)
+    setOurData(newData)
+  }, [])
   return (
     <>
       <div>
@@ -14,11 +23,23 @@ export const Grocery = () => {
           </div>
           <div>
             <ul>
-              <li>
-                <input type="checkbox" />
-                <p>Hallo world</p>
-                <button>Delete</button>
-              </li>
+              {ourData.map((data) => {
+                return (
+                  <li key={data.id}>
+                    <input
+                      type="checkbox"
+                      checked={data.checked ? true : false}
+                      value={checkBox}
+                      onChange={(e) => {
+                        setCheckBox(e.target.value)
+                      }}
+                    />
+
+                    <p>{data.text}</p>
+                    <button>Delete</button>
+                  </li>
+                )
+              })}
             </ul>
           </div>
         </div>
---
+++ src/component/hooks/Data.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Data = () => {
+  const getData = [
+    {
+      id: 1,
+      text: 'hey',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'hey23',
+      checked: false,
+    },
+  ]
+  return getData
+}
