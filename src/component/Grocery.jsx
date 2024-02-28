8794898e895715cfe274e9934ee1daa0b42e44d6
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-28 04:37:41 +0100

    added more styling

--- src/component/Grocery.jsx
+++ src/component/Grocery.jsx
@@ -19,11 +19,11 @@ export const Grocery = () => {
 
   return (
     <>
-      <div>
-        <div>
-          <div>
+      <div className="flex flex-col text-center items-center justify-center h-[100vh] w-[100vw]">
+        <div className="h-[500px] w-[360px] bg-zinc-400">
+          <div className=" h-[15%] w-[100%] flex flex-col text-center items-center justify-center gap-2">
             <h1>Grocery List</h1>
-            <div>
+            <div className="flex flex-row text-center items-center justify-evenly w-[100%]">
               <input
                 type="text"
                 placeholder="add somet..."
@@ -45,30 +45,34 @@ export const Grocery = () => {
             <ul>
               {ourData.map((data, index) => {
                 return (
-                  <li key={data.id}>
-                    <input
-                      data-index={index}
-                      type="checkbox"
-                      checked={data.checked ? true : false}
-                      onChange={(e) => {
-                        changeCheckBox(
-                          e.currentTarget.getAttribute('data-index'),
-                          ourData,
-                          setOurData,
-                          setNewData
-                        )
-                      }}
-                    />
-
-                    <p
-                      className={
-                        data.checked
-                          ? ' text-blue-400 line-through'
-                          : 'text-black'
-                      }
-                    >
-                      {data.text}
-                    </p>
+                  <li
+                    key={data.id}
+                    className="flex flex-row text-center items-center justify-between p-4 w-[100%] h-[50px]"
+                  >
+                    <div className="flex flex-row w-[50%] text-center items-center justify-start gap-4">
+                      <input
+                        data-index={index}
+                        type="checkbox"
+                        checked={data.checked ? true : false}
+                        onChange={(e) => {
+                          changeCheckBox(
+                            e.currentTarget.getAttribute('data-index'),
+                            ourData,
+                            setOurData,
+                            setNewData
+                          )
+                        }}
+                      />
+                      <p
+                        className={
+                          data.checked
+                            ? ' text-blue-400 line-through'
+                            : 'text-black'
+                        }
+                      >
+                        {data.text}
+                      </p>
+                    </div>
                     <button
                       onClick={() => {
                         DeleteItem(data.id, ourData, setOurData)
